e5cd30c6f7b284ec9faffaaa1167fdedcbb8511b
Devansh-bharadwaj <devanshbharadwaj1@gmail.com>
2023-05-18 18:41:05 +0530

    updated logout

---
+++ Client/src/components/add-new-btn/Addnewbtn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BsBookmarkPlus } from 'react-icons/bs';
+import './Addnewbtn.css';
+
+function Addnewbtn() {
+  return (
+    <div>
+        <button className='addnew-btn'><BsBookmarkPlus />ADD NEW</button>
+    </div>
+  )
+}
+
+export default Addnewbtn
