182b10fc25db2769580ba40ff229b2dfadd4ec8c
Devansh-bharadwaj <devanshbharadwaj1@gmail.com>
2023-05-18 18:59:50 +0530

    updated

--- Client/src/components/amenities/Amenities.js
+++ Client/src/components/amenities/Amenities.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Mainpanelnav from '../mainpanel-header/Mainpanelnav'
+import Addnewbtn from '../add-new-btn/Addnewbtn'
 
 function Amenities() {
   return (
     <div  className='mx-5 mt-3'>
       <Mainpanelnav />
+      <Addnewbtn />
     </div>
   )
 }
