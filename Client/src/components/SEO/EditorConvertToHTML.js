a743c06f72586a28e57509b053b8fd234f976c05
Devansh-bharadwaj <devanshbharadwaj1@gmail.com>
2023-05-30 11:11:02 +0530

    updated

---
+++ Client/src/components/SEO/EditorConvertToHTML.js
@@ -0,0 +1,34 @@
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState, convertToRaw } from "draft-js";
+import { React, Component } from "react";
+import draftToHtml from 'draftjs-to-html';
+import "./Seo.css";
+
+export default class EditorConvertToHTML extends Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+  }
+
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    })
+  }
+
+  render() {
+    const {editorState} = this.state;
+    // console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+    return (
+      <div>
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+      </div>
+    );
+  }
+}
